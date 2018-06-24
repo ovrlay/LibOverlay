c8257109815595229d5e4ed8c0105bb562a73ab0
Robin Appelman <robin@icewind.nl>
2018-06-24 18:23:26 +0200

    Add basic README and sample

---
+++ OverlaySample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Random = System.Random;
+using LibOverlay;
+
+class OverlaySample : MonoBehaviour
+{
+    public Texture OverlayTexture;
+    public float Alpha = 1;
+    public static Random Rand = new Random();
+    public static string Key { get { return "unity:" + Application.companyName + "." + Application.productName + "." + Rand.Next(); } }
+    private Overlay overlay;
+
+    public void OnEnable()
+    {
+        overlay = new Overlay(Key + gameObject.GetInstanceID(), gameObject.name)
+        {
+            OverlayTexture = OverlayTexture,
+            Alpha = Alpha
+        };
+        overlay.TrackedDevice = TrackedDevice.GetController(TrackedDevice.ControllerRole.RightHand);
+        overlay.Enable();
+        overlay.Transform = gameObject.transform;
+    }
+
+    public void OnDisable()
+    {
+        overlay.Disable();
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# LibOverlay
+
+Simplify creation of OpenVR overlays in unity
+
+# Usage
+
+- Add included scripts to Unity Project
+- Attach sample overlay to game object
+- Set texture to render as overlay
