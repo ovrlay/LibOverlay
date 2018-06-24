ed4bde675ce1323526ee1e5681e2df937ff491b1
Robin Appelman <robin@icewind.nl>
2018-06-24 19:42:29 +0200

    initialize steamvr as overlay

--- OverlaySample.cs
+++ OverlaySample.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using Valve.VR;
 using Random = System.Random;
 using LibOverlay;
+using Valve.VR;
 
 class OverlaySample : MonoBehaviour
 {
@@ -15,6 +15,15 @@ class OverlaySample : MonoBehaviour
 
     public void OnEnable()
     {
+        var error = EVRInitError.None;
+        OpenVR.Init(ref error, EVRApplicationType.VRApplication_Overlay);
+        if (error != EVRInitError.None)
+        {
+            Debug.Log(error.ToString());
+            enabled = false;
+            return;
+        }
+
         overlay = new Overlay(Key + gameObject.GetInstanceID(), gameObject.name)
         {
             OverlayTexture = OverlayTexture,
